cfda5c9ea6b565baf1a42a419415d7411cafafb8
yonko3rbco <57264505+yonko3rbco@users.noreply.github.com>
2019-11-01 19:50:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("637240970949296128")
 setInterval(function() {
-channel.send(`اكو قحاب بالسيرفرhttp://cdn-webimages.wimages.net/050810e1feb1c961748111c698508310cc5071-wm.jpg?v=3`);
+channel.send(` 777  فووووووووق اكو قحاب بالسيرفرhttp://cdn-webimages.wimages.net/050810e1feb1c961748111c698508310cc5071-wm.jpg?v=3`);
 }, 30)
 })
 
